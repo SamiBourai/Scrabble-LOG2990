faa8f3f34d69825d1c84c62f482f82a9ec0e76b7
yaaoua <yanick.aouchiche@polymtl.ca>
2021-09-13 16:46:27 -0400

    finishing point d'entrée

--- client/src/app/modal-scrable-classique/modal-scrable-classique.component.ts
+++ client/src/app/modal-scrable-classique/modal-scrable-classique.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 @Component({
-  selector: 'app-modal-scrable-classique',
-  templateUrl: './modal-scrable-classique.component.html',
-  styleUrls: ['./modal-scrable-classique.component.scss']
+    selector: 'app-modal-scrable-classique',
+    templateUrl: './modal-scrable-classique.component.html',
+    styleUrls: ['./modal-scrable-classique.component.scss'],
 })
-export class ModalScrableClassiqueComponent implements OnInit {
-
-  constructor() { }
-  ngOnInit(): void {
-  }
-
+export class ModalScrableClassiqueComponent {
+    @ViewChild('button', { static: false }) private button: ElementRef<HTMLElement>;
+    mainPageComponent: MainPageComponent;
+    // constructor() {}
+    closeWindow(): void {
+        console.log(this.button);
+        this.button.nativeElement.style.display = 'none';
+    }
 }
