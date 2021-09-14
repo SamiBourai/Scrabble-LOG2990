0d0c5b52b53ed91f4bbe8bbe5449f610ce5a8203
yaaoua <yanick.aouchiche@polymtl.ca>
2021-09-14 17:18:48 -0400

    pop-up for mode classique

--- client/src/app/modal-scrable-classique/modal-scrable-classique.component.ts
+++ client/src/app/modal-scrable-classique/modal-scrable-classique.component.ts
@@ -1,16 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { Component, ViewChild } from '@angular/core';
+// import { FormBuilder } from '@angular/forms';
+// import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 @Component({
     selector: 'app-modal-scrable-classique',
     templateUrl: './modal-scrable-classique.component.html',
     styleUrls: ['./modal-scrable-classique.component.scss'],
 })
 export class ModalScrableClassiqueComponent {
-    @ViewChild('button', { static: false }) private button: ElementRef<HTMLElement>;
-    mainPageComponent: MainPageComponent;
-    // constructor() {}
+    // mainPageComponent: MainPageComponent;
+    // @ViewChild('button', { static: false }) button: ElementRef<HTMLElement>;
+    @ViewChild('button') button: { nativeElement: { click: () => void } };
+    // constructor(button: HTMLElement) {
+    //     t
+    // }
+    // ngOnInit() {}
+    // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+    // ngAfterViewInit() {
+    //     setTimeout(() => {this.button.nativeElement.click();
+    //         console.log(this.abc.nativeElement.innerText);
+    //     }, 1000);
+    // }
     closeWindow(): void {
-        console.log(this.button);
-        this.button.nativeElement.style.display = 'none';
+        if (this.button) this.button.nativeElement.click();
     }
 }
