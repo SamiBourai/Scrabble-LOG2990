36d4b0034afbf2793c48dd89fc96bfe00a417469
Gaya Bessaih <gaya.bessaih@polymtl.ca>
2021-11-19 21:02:01 -0500

    changer les dialog

--- client/src/app/components/modals/modal-user-name/modal-user-name.component.ts
+++ client/src/app/components/modals/modal-user-name/modal-user-name.component.ts
@@ -42,6 +42,9 @@ export class ModalUserNameComponent implements OnInit {
     modes: string[] = MODES;
     chosenMode: string = MODES[DEFAULT_MODE];
     chooseSoloMode: boolean = false;
+    toolTip: string =
+        "(1) Le nom ne doit pas comporter de caractère speciaux, Ex: #@*...! (2) Le nom ne doit pas contenir d'espace (3) Le nom doit avoir au min 8 caractere et max 15";
+
     constructor(
         private dialogRef: MatDialog,
         public userService: UserService,
