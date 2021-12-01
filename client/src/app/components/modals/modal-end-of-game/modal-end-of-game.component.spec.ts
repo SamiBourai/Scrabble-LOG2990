a623269d0657e6606b00210b2300df3a476f07fd
Gaya <gaya.bessaih@polymtl.ca>
2021-11-30 19:56:22 -0500

    modal end of game tested

--- client/src/app/components/modals/modal-end-of-game/modal-end-of-game.component.spec.ts
+++ client/src/app/components/modals/modal-end-of-game/modal-end-of-game.component.spec.ts
@@ -1,13 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+//import { UserService } from '@app/services/user.service';
+//import { of } from 'rxjs';
 
 import { ModalEndOfGameComponent } from './modal-end-of-game.component';
 
 describe('ModalEndOfGameComponent', () => {
     let component: ModalEndOfGameComponent;
     let fixture: ComponentFixture<ModalEndOfGameComponent>;
+    // let userServiceSpy: jasmine.SpyObj<UserService>;
+
+    // beforeEach(() => {
+    //     userServiceSpy = jasmine.createSpyObj('UserService', ['playMode', 'isBonusBox', 'initiliseUsers','setVrName']);
+    
+    // });
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -26,10 +33,23 @@ describe('ModalEndOfGameComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('ngOnInit', () => {
-        const data = true;
-        const spy = spyOn<any>(component.multiplayerService, 'winnerOfGame').and.returnValue(of(data));
-        component.ngOnInit();
-        expect(spy).not.toHaveBeenCalled();
+    // it('ngOnInit', () => {
+    //     const data = true;
+    //     const spy = spyOn<any>(component.multiplayerService, 'winnerOfGame').and.returnValue(of(data));
+    //     component.ngOnInit();
+    //     expect(spy).not.toHaveBeenCalled();
+    // });
+
+    it('joinVrPlayer dans if', () => {
+        component['userService'].playMode = 'joinMultiplayerGame';
+        component.joinVrPlayer();
+        expect(component['userService'].setJoinAsReal()).toHaveBeenCalled();
+    });
+
+    it('joinVrPlayer else', () => {
+        component['userService'].playMode = 'none';
+        component.joinVrPlayer();
+        expect(component['userService'].playMode).toBe('soloGame');
+        expect(component['userService'].endOfGame).toBeFalse();
     });
 });
